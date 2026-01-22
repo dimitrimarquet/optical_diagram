343055dfc366726cfb27e8b69dc4d37aa701a550
Théo Soler <theo.soler@mines-paristech.fr>
2026-01-22 21:46:09 +0100

    correction bugs laser + methode arctan + commenting

--- Optics_commented.py
+++ Optics_commented.py
@@ -29,7 +29,6 @@ class Mirror(Instruments):
         orientation (tuple): Vecteur d'orientation du miroir (mx, my).
         reflectivity (float): Coefficient de réflectivité du miroir (0 à 1).
     """
-
     def __init__(self, orientation_vector, reflectivity):
         self.orientation = orientation_vector
         self.reflectivity = reflectivity
@@ -48,20 +47,15 @@ class Mirror(Instruments):
         wvl = rayon.wavelength
         mx, my = self.orientation
         kx_init, ky_init = k_init
-        
-        #calculation of theta, angle of inclination of the mirror with respect to x
         if mx != 0:
-            theta = -np.pi/2 + np.arctan(my/mx) 
+            theta = -np.pi/2 + np.arctan(my/mx)
         else :
             theta = 0
-
-        #calculation of the reflected k vector k_out
         kx_theta = kx_init*np.cos(2*theta) + ky_init*np.sin(2*theta)
         ky_theta = - ky_init*np.cos(2*theta) + kx_init*np.sin(2*theta)
         k_out = (kx_theta, ky_theta)
-
         return Laser(wvl, k_out)
-   
+
     def display(self, position, length = 2):
         """
         Calcule les coordonnées pour afficher le miroir sous forme de segment.
@@ -73,17 +67,12 @@ class Mirror(Instruments):
         Returns:
             tuple: Deux tableaux numpy (x_array, y_array) représentant les extrémités du miroir.
         """
-
         x_position, y_position = position
         mx, my = self.orientation
-
-        #calculation of theta, angle of inclination of the mirror with respect to x
         if mx != 0:
             theta = - np.pi/2 + np.arctan(my/mx)
         else :
             theta = 0
-        
-        #calculation of arrays (x_array, y_array) representing the mirror in (x,y) plane as a segment
         x_max = x_position + np.cos(theta)*length/2
         x_min = x_position - np.cos(theta)*length/2
 
@@ -92,7 +81,6 @@ class Mirror(Instruments):
 
         x_array = np.linspace(x_min, x_max, 10)
         y_array = np.linspace(y_min, y_max, 10)
-
         return (x_array, y_array)
 
 class TableOptique(dict):
@@ -102,7 +90,6 @@ class TableOptique(dict):
     Attributs:
         size (tuple): Taille (largeur, hauteur) de la table.
     """
-
     def __init__(self, size):
         self.size = size
 
@@ -120,18 +107,16 @@ class TableOptique(dict):
         """
         Affiche tous les instruments optiques présents sur la table.
         """
-
         for optics in self.keys():
             x_array, y_array = optics.display(self[optics])
             plt.plot(x_array, y_array, color = "red")
-        
-        #matplotlib adjustments
         xlim, ylim = self.size
         plt.xlim(-xlim/2, xlim/2)
         plt.ylim(-ylim/2, ylim/2)
         plt.gca().set_aspect('equal', adjustable='box')
-    
-    def path_laser(self, laser, position_init):
+        # plt.show()
+
+    def path_laser(self, laser, position_init, epsilon = 0.005):
         """
         Calcule le trajet d'un faisceau laser sur la table optique.
 
@@ -144,17 +129,13 @@ class TableOptique(dict):
             tuple: Deux listes (x_array, y_array) représentant le trajet du faisceau.
         """
         ray = laser
-        x_init, y_init = position_init #position of the source of the laser
-        x_size, y_size = self.size #dimensions of the table
-        x_array = [x_init] #array of x coordinates needed for tracing the laser path
-        y_array = [y_init] #array of y coordinates needed for tracing the laser path
-        d_pos = np.sqrt(x_size**2 + y_size**2)*2 #max distance to calculate
-        used_optics = [] #to avoid the program to loop on the same optics -> to modify for cavities !!
-
-        i = 0
-        while np.abs(x_array[-1]) < x_size/2 and np.abs(y_array[-1]) < y_size/2 and i < 100:
-
-            #finding of the potential optics that could be touched by the laser considering its initial k vector, using the cone approach
+        x_init, y_init = position_init
+        x_size, y_size = self.size
+        x_array = [x_init]
+        y_array = [y_init]
+        d_pos = 15
+        used_optics = []
+        while np.abs(x_array[-1]) < x_size/2 and np.abs(y_array[-1]) < y_size/2 :
             list_optics_in_cone = []
             kx_init, ky_init = ray.k_vector
             norm_init = np.sqrt(kx_init**2 + ky_init**2)
@@ -168,35 +149,23 @@ class TableOptique(dict):
                 i_ymax_optics = np.argmax(y_array_optics)
                 i_ymin_optics = np.argmin(y_array_optics)
 
-                kmax_x, kmax_y = (x_array_optics[i_xmax_optics] - x_init, y_array_optics[i_xmax_optics] - y_init)
-                kmin_x, kmin_y = (x_array_optics[i_xmin_optics] - x_init, y_array_optics[i_xmin_optics] - y_init)
+                kmax_x, kmax_y = (x_array_optics[i_xmax_optics] - x_init, y_array_optics[i_ymax_optics] - y_init)
+                kmin_x, kmin_y = (x_array_optics[i_xmin_optics] - x_init, y_array_optics[i_ymin_optics] - y_init)
 
                 norm_max = np.sqrt(kmax_x**2 + kmax_y**2)
                 norm_min = np.sqrt(kmin_x**2 + kmin_y**2)
+                testy = kmin_y/norm_min >= ky_init/norm_init >= kmax_y/norm_max or kmax_y/norm_max >= ky_init/norm_init >= kmin_y/norm_min
+                test_optics_in_cone = kmin_x/norm_min >= kx_init/norm_init >= kmax_x/norm_max or kmin_x/norm_min <= kx_init/norm_init <= kmax_x/norm_max
 
-                #test to be on the right side of the cone if scalar product positive
-                scalar_max = np.dot(np.array([kmax_x, kmax_y])/norm_max, np.array([kx_init, ky_init])/norm_init)
-                scalar_min = np.dot(np.array([kmin_x, kmin_y])/norm_min, np.array([kx_init, ky_init])/norm_init)
-
-                #test to be in the cone if the angle with respect to x is between the two
-                test_max = np.arctan(kmin_y/kmin_x) <= np.arctan(ky_init/kx_init) <= np.arctan(kmax_y/kmax_x)
-                test_min = np.arctan(kmin_y/kmin_x) >= np.arctan(ky_init/kx_init) >= np.arctan(kmax_y/kmax_x)
-                test_optics_in_cone = scalar_max >= 0 and scalar_min >= 0 and (test_max or test_min)
-
-                if test_optics_in_cone and optics not in used_optics[-1:]:
+                if test_optics_in_cone and testy and optics not in used_optics:
                     list_optics_in_cone.append(optics)
-
-            
             if len(list_optics_in_cone) != 0:
-
-                #discrimination of the optics that is actually going to be touched (closest from the initial position)
                 list_position_optics_in_cone = [self[el] for el in list_optics_in_cone]
+                print(list_position_optics_in_cone)
                 list_distance_optics_in_cone = [np.sqrt((el[0] - x_array[-1])**2 + (el[1] - y_array[-1])**2) for el in list_position_optics_in_cone]
 
                 optics = list_optics_in_cone[np.argmin(list_distance_optics_in_cone)]
                 position_optics = self[optics]
-
-                #calculation of the intersection point and reflection except in the case where the laser comes on the laser side
                 x_optics, y_optics = position_optics
                 x_array_optics, y_array_optics = optics.display(position_optics)
 
@@ -211,12 +180,11 @@ class TableOptique(dict):
                 norm_max = np.sqrt(kmax_x**2 + kmax_y**2)
                 norm_min = np.sqrt(kmin_x**2 + kmin_y**2)
 
-                if x_array_optics[i_xmax_optics] - x_array_optics[i_xmin_optics] != 0: #if the mirror is not along y
+                if x_array_optics[i_xmax_optics] - x_array_optics[i_xmin_optics] != 0:
                     coef_miroir = (y_array_optics[i_xmax_optics] - y_array_optics[i_xmin_optics])/(x_array_optics[i_xmax_optics] - x_array_optics[i_xmin_optics])
                     coef_laser = ky_init/kx_init
 
-                    if coef_miroir-coef_laser != 0: #if the laser does not land on the mirror's side
-                        
+                    if coef_miroir-coef_laser != 0:
                         x_intersect = (y_init-y_optics + coef_miroir*x_optics - coef_laser*x_init)/(coef_miroir-coef_laser)
                         y_intersect = coef_miroir*(x_intersect-x_optics) + y_optics
                         x_array.append(x_intersect)
@@ -224,24 +192,21 @@ class TableOptique(dict):
                         x_init, y_init = x_intersect, y_intersect
                         ray = optics.reflect(ray)
                         used_optics.append(optics)
-                        print("used_optics", self[used_optics[-1]])
 
                     else :
                         x_array.append(x_optics)
                         y_array.append(y_optics)
                         return (x_array, y_array)
-
-                else : #if the mirror is along y
+                else :
                     used_optics.append(optics)
                     print("not implemented yet...")
-                        
+
             else:
                 x_array.append(x_array[-1] + d_pos*kx_init)
                 y_array.append(y_array[-1] + d_pos*ky_init)
-            i += 1
-                                    
+
         return (x_array, y_array)
-    
+
     def draw_laser(self, laser, position_source):
         """
         Affiche le trajet d'un faisceau laser sur la table optique.
@@ -254,28 +219,21 @@ class TableOptique(dict):
         plt.plot(x_array, y_array)
         # plt.show()
 
-k = (1,1)
+# Exemple d'utilisation
+k = (-5,3.5)
 rayon = Laser(620, k)
-# mirror1 = Mirror((0,1), 1)
-# mirror2 = Mirror((1,0.001), 1)
-# mirror3 = Mirror((-1,159), 1)
-# mirror4 = Mirror((150,1), 1)
+mirror1 = Mirror((0,1), 1)
+mirror2 = Mirror((1,0.001), 1)
+mirror3 = Mirror((-1,159), 1)
+mirror4 = Mirror((150,1), 1)
 table = TableOptique((20,20))
-# table.add(mirror3, (1.6,-5.5))
+table.add(mirror3, (1.6,-5.5))
+table.add(mirror1, (-5,4))
+table.add(mirror2, (-8.7,1.69))
+table.add(mirror4, (-3.81,-1.82))
 
-# table.add(mirror1, (-5,4))
-# table.add(mirror2, (-8.7,1.69))
-# table.add(mirror4, (-3.81,-1.82))
-
-#test_cavity
-
-mirror1 = Mirror((1,1), 1)
-mirror2 = Mirror((1,1), 1)
-
-table.add(mirror1, (4,4))
-table.add(mirror2, (-4,-4))
 table.draw()
-table.draw_laser(rayon, (0,0))
+table.draw_laser(rayon, (-1,1))
 # print(mirror.reflect(rayon).k_vector)
 
 plt.show()
